e584b43c418bb74820f114278ae3718878cd7f17
dslackw <d.zlatanidis@gmail.com>
2014-06-11 04:06:03 +0300

    updated for version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name = 'pysed',
-    version = "0.0.1",
+    version = "0.0.2",
     description = "Utility that parses and transforms text",
     keywords = ["python", "sed", "unix", "linux", "text", 
 		"stream", "editor"],
