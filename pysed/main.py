3781636253b8152243b32c1223e03b6ad7ee2cb8
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-03-28 12:10:15 +0200

    rename num to depth

--- pysed/main.py
+++ pysed/main.py
@@ -40,22 +40,22 @@ def print_file(text):
         print("{0}".format(line))
 
 
-def replace_text(old, new, num, text):
+def replace_text(old, new, depth, text):
     """ return replace text """
     find = re.findall(old, text)
-    if num == "":
-        num = len(text)
+    if depth == "":
+        depth = len(text)
     for p in set(find):
-        text = text.replace(p, new, int(num))
+        text = text.replace(p, new, int(depth))
     return text.rstrip()
 
 
 def choice(pattern, reg):
     """ return results """
-    num = "".join(re.findall(r"\d+", pattern[0]))
+    depth = "".join(re.findall(r"\d+", pattern[0]))
     if len(pattern) == 4:
         if pattern[0].startswith("s"):
-            return replace_text(pattern[1], pattern[2], num, reg)
+            return replace_text(pattern[1], pattern[2], depth, reg)
     return ""
 
 
