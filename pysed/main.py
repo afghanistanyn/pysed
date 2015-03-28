e5fa36f34f49c7e865b8b6c7cb642bea7f373145
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-03-28 11:36:37 +0200

    update

--- pysed/main.py
+++ pysed/main.py
@@ -10,7 +10,7 @@
 
 # https://github.com/dslackw/pysed
 
-# Slpkg is free software: you can redistribute it and/or modify
+# Pysed is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
