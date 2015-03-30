839ee11c8677fb9411c8b0cae4567e0b47777717
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-03-30 13:57:44 +0300

    update base

--- pysed/main.py
+++ pysed/main.py
@@ -24,20 +24,20 @@
 
 import re
 import sys
+from utils import (
+    fix_pattern,
+    write_file,
+    print_file
+)
 
-
-def write_file(text, reg):
-    """ write a new file """
-    with open(reg, "w") as f:
-        for line in text.splitlines():
-            f.write(line + "\n")
-        f.close()
-
-
-def print_file(text):
-    """ print text """
-    for line in text.splitlines():
-        print("{0}".format(line))
+__all__ = "pysed"
+__author__ = "dslackw"
+__copyright__ = 2014 - 2015
+__version_info__ = (0, 5, 0)
+__version__ = "{0}.{1}.{2}".format(*__version_info__)
+__license__ = "GNU General Public License v3 (GPLv3)"
+__email__ = "d.zlatanidis@gmail.com"
+__website__ = "https://github.com/dslackw/pysed"
 
 
 def replace_text(old, new, depth, text):
@@ -45,23 +45,76 @@ def replace_text(old, new, depth, text):
     find = re.findall(old, text)
     if depth == "":
         depth = len(text)
+    if old == "^":
+        return ("".join(
+            [new + line + "\n" for line in text.splitlines()]).rstrip())
     for p in set(find):
         text = text.replace(p, new, int(depth))
     return text.rstrip()
 
 
-def choice(pattern, reg):
+def find_line(find, text):
+    """ find pattern and return lines """
+    lines = ""
+    for line in text.splitlines():
+        if find in line:
+            lines += line + "\n"
+    return lines.rstrip()
+
+
+def choice(pattern, text):
     """ return results """
-    depth = "".join(re.findall(r"\d+", pattern[0]))
     if len(pattern) == 4:
         if pattern[0].startswith("s"):
-            return replace_text(pattern[1], pattern[2], depth, reg)
-    return ""
+            depth = "".join(re.findall(r"\d+", pattern[0]))
+            return replace_text(pattern[1], pattern[2], depth, text)
+    if len(pattern) == 3:
+        if pattern[0].startswith("l"):
+            return find_line(pattern[1], text)
+    usage()
+
+
+def options():
+    """ print arguments options """
+    args = [
+        "Usage: %s '{left}/pattern/{right}' [input-file]\n" % __all__,
+        "Left options:",
+        "  s/           search and replace text",
+        "  l/           find pattern and print lines",
+        "Right option:",
+        "  /p           print text",
+        "  /w           write to a file\n",
+    ]
+    for opt in args:
+        print opt
+    sys.exit()
+
+
+def usage():
+    """ print arguments usage """
+    usg = [
+        "usage: {0} [-h] [-v]".format(__all__),
+        "             left  [s/, l/]",
+        "             right [/p, /w]\n"
+    ]
+    for opt in usg:
+        print opt
+    sys.exit()
+
+
+def arguments(args):
+    """ optional arguments """
+    if len(args) == 1:
+        if args[0] in ["-h", "--help"]:
+            options()
+        else:
+            usage()
 
 
 def main():
     args = sys.argv
     args.pop(0)
+    arguments(args)
 
     if len(sys.argv) > 1:
         f = open(args[1])
@@ -72,21 +125,28 @@ def main():
         except KeyboardInterrupt:
             print("")
             sys.exit()
-    try:
-        if len(args) >= 1 and len(args) <= 2:
-            pattern = args[0].split("/")
-            text = choice(pattern, data)
 
-            if len(args) == 2:
-                option = pattern[3]
-                if option == "p":
-                    print_file(text)
-                elif option == "w":
-                    write_file(text, args[1])
-            elif len(args) == 1:
+    # try:
+    if len(args) >= 1 and len(args) <= 2:
+        pattern = fix_pattern(args[0].split("/"))
+        text = choice(pattern, data)
+
+        if len(pattern) == 4:
+            option = pattern[3]
+            if option == "p":
                 print_file(text)
-    except IndexError:
-        sys.exit()
+            elif option == "w":
+                write_file(text, args[1])
+        elif len(pattern) == 3:
+            option = pattern[2]
+            if option == "p":
+                print_file(text)
+        else:
+            usage()
+    else:
+        usage()
+    # except IndexError:
+    #     sys.exit()
 
 if __name__ == "__main__":
     main()
