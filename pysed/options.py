e7672ce9f40c11290d2fdeb72a5d21037f3de3a4
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-06-17 04:35:49 +0300

    Fix modules for Python 3

--- pysed/options.py
+++ pysed/options.py
@@ -22,7 +22,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import sys
-from __metadata__ import (
+from pysed.__metadata__ import (
     __prog__,
     __email__,
     __version__,
